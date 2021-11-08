98eee8acc9090f0ea81808e66c99effc90383d44
alineskamo <lineskity@gmail.com>
2021-11-08 10:07:38 -0300

    :tada: First commit

---
+++ src/main/java/com/br/elit/elitProducer/ElitProducerApplication.java
@@ -0,0 +1,13 @@
+package com.br.elit.elitProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElitProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElitProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/elit/elitProducer/ElitProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.elit.elitProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElitProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
